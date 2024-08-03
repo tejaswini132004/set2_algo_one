a75219b5789aff7ae497b1a94bd2f09c54294011
tejaswini132004 <138596101+tejaswini132004@users.noreply.github.com>
2024-08-03 14:28:52 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,10 @@
+let prices=[7,1,5,3,6,4];
+let profit;
+for(let i=0;i<prices.length;i++){
+ if(prices[2]==0 && prices==[4]){
+     console.log("0");
+ }
+ else{
+    console.log("Profit : ",prices[4]-prices[1]);
+ }
+}
